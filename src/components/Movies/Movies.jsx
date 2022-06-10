51db743e2c85d6c1a8df286665283a83966ee278
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-06-10 22:23:49 +0530

    Redux setup

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  useMediaQuery,
+  Box,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { useGetMoviesQuery } from "../../services/TMDB";
 
 const Movies = () => {
+  const { data } = useGetMoviesQuery();
+  console.log(data);
+
   return <div>Movies</div>;
 };
 
